150dacb11280d546610a2ce732eb406a6427d01d
arikshin <arik.shin@gmail.com>
2026-05-03 15:18:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE = 'family-shopping-v1';
-const FILES = ['./', './index.html'];
+const CACHE = 'family-shopping-v2';
+const FILES = [
+  '/family-groceries/',
+  '/family-groceries/index.html',
+  '/family-groceries/icon-192.png',
+  '/family-groceries/icon-512.png',
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -14,7 +19,16 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  // רק בקשות GET
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(res => {
+        // שמור עותק בcache
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
